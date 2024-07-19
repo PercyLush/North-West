a534ea4b301e087d19769507b566747fccd881e0
Bheki Lushaba <bheki@gradesmatch.com>
2024-07-19 08:18:50 +0200

    add

--- compare_and_extend.py
+++ compare_and_extend.py
@@ -1,7 +1,7 @@
 import json
 
-path1 = "path_to_first_json_file.json"
-path2 = "path_to_second_json_file.json"
+path2 = "C:\\Users\\Bheki Lushaba\\course-data\\CourseData_Final\\North West University.json"
+path1 = "C:\\Users\\Bheki Lushaba\\Desktop\\North-West\\NaturalScienceFinal.json"
 
 with open(path1, "r", encoding="utf-8") as file1, open(path2, "r", encoding="utf-8") as file2:
     data1 = json.load(file1)
@@ -12,10 +12,11 @@ with open(path1, "r", encoding="utf-8") as file1, open(path2, "r", encoding="utf
 
     # Append items from data1 to data2 if their code is not already present in data2
     for item1 in data1:
-        if item1["Code"] not in codes_in_data2:
-            data2.append(item1)
+        if "Code" in item1:
+            if item1["Code"] not in codes_in_data2:
+                data2.append(item1)
 
 # Save the merged data to a new file or overwrite the existing one
-merged_path = "merged_json_file.json"
+merged_path = "C:\\Users\\Bheki Lushaba\\course-data\\CourseData_Final\\North West University.json"
 with open(merged_path, "w", encoding="utf-8") as merged_file:
-    json.dump(data2, merged_file, ensure_ascii=False, indent=4)
+    json.dump(data2, merged_file, ensure_ascii=False, indent=2)
